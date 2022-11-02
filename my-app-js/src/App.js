083e90ebb4b7b2452cb87360efb7baf9ef91b442
Pavel <pavel07007@gmail.com>
2022-11-02 15:22:34 +0300

    add my-app-on js

---
+++ my-app-js/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.
+import Header from "./Header";
+import Tehnolohians from "./Tehnolohians";
+
+
+const App = () =>{
+  return (
+    <div>
+    <div>
+      <Header />
+    </div>
+    <Tehnolohians />
+    </div>
+  );
+}
+export default App;
---
+++ my-app-js/src/Tehnolohians.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Tehnolohians =()=>{
+    return <div>
+        <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+        </ul>
+    </div>
+}
+export default Tehnolohians;
