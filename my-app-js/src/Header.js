5d49c2280a733695ee2968b6edec764ae399ecbe
Pavel <pavel07007@gmail.com>
2022-11-03 15:08:08 +0300

    change header

--- my-app-js/src/Header.js
+++ my-app-js/src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Header = ()=>{
     return(<div>
-        <a href='s'>Home</a>
+        <a href='ss'>Home</a>
         <a href='s'>News Feed</a>
         <a href='s'>Message</a>
     </div>)
